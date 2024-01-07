2ee407b20beed4cd213d917f465ca9f7d4ecf272
Monica Guerra <monik_flute@hotmail.com>
2024-01-07 18:30:30 +0100

    ej json 1

---
+++ JSON1.js
@@ -0,0 +1,18 @@
+//Convert the developer object into JSON
+
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+let strg = JSON.stringify(developer);
+console.log(strg);
+let jsonobject =JSON.parse(strg);
+console.log(jsonobject)
+
+// Print developer as JSON object
