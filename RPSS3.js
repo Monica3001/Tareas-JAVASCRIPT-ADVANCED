9dfb1c755450fb84e26bd817eeceb2bebdb3c688
Monica Guerra <monik_flute@hotmail.com>
2024-01-07 16:16:54 +0100

    ej12

---
+++ RPSS3.js
@@ -0,0 +1,22 @@
+//Perform a code refactoring by using the Rest Operator.
+// The output of the console.log must be the same.
+
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+/*const id = person.id;
+const personInfo = {
+  firstName: person.firstName,
+  lastName: person.lastName,
+  age: person.age
+}; */
+
+const id = person.id;
+let personInfo = {...person}; 
+delete personInfo.id;
+
+console.log(id, personInfo);
