950040fbda1849b8ef5b6b5858f09334872fe45f
Monica Guerra <monik_flute@hotmail.com>
2024-01-09 14:29:11 +0100

    ejer 20 callbacks 2

---
+++ callbacks2.js
@@ -0,0 +1,14 @@
+//Write a function called repeatHello which takes a callback as it's parameter. 
+//The callback function will simply print "Hello". 
+//The repeatHello function will have to execute the callback function with an interval of 1 second.
+// The callback function must be an arrow function.
+
+
+
+function repeatHello () {
+
+   setTimeout(() => console.log("hello"), 1000);
+   
+  }
+ 
+repeatHello()
